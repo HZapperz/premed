54ad55d8c1dcab7a2aa329f0c65fa8f5d61bff2c
HZapperz <46976026+HZapperz@users.noreply.github.com>
2023-09-01 16:38:51 -0500

    2nd commit

--- aayush.py
+++ aayush.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import pdfplumber
-import openai
 import pyttsx3
 import time
+from pdf2image import convert_from_path
+from PyPDF2 import PdfReader
+import openai
+import os
 
 # Function to speak text
 def speak_text(text):
@@ -11,14 +13,12 @@ def speak_text(text):
     engine.runAndWait()
 
 def read_pdf(file_path):
-    slides_text = []
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            slides_text.append(page.extract_text())
+    reader = PdfReader(file_path)
+    slides_text = [page.extract_text() for page in reader.pages]
     return slides_text
 
 def generate_notes(slides_text):
-    openai.api_key = "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"
+    
     generated_notes = []
     for slide in slides_text:
         prompt = f"Create study notes for the following slide content:\n{slide}"
@@ -30,27 +30,61 @@ def generate_notes(slides_text):
         generated_notes.append(response.choices[0].text.strip())
     return generated_notes
 
+def convert_pdf_to_images(pdf_path):
+    return convert_from_path(pdf_path)
+
 def main():
     st.title('Automated Study Notes')
+    uploaded_file = st.file_uploader("Upload a PDF file", type=['pdf'])
 
-    pdf_path = st.text_input('Enter the path to your slides PDF:', 'test1.pdf')
-    if st.button('Generate and Speak Notes'):
-        slides_text = read_pdf(pdf_path)
-        generated_notes = generate_notes(slides_text)
+    if uploaded_file:
+        with open("test1.pdf", "wb") as f:
+            f.write(uploaded_file.read())
+
+        st.write("Uploaded PDF file successfully!")
+
+        page_range = st.text_input("Which pages would you like to go over? (e.g., 1,2,3 or 1-3)")
         
-        for i, note in enumerate(generated_notes):
-            # Using markdown to highlight the current note
-            notes_to_show = [f"**{note}**" if j == i else note for j, note in enumerate(generated_notes)]
-            st.write("---")
-            st.markdown("\n".join(notes_to_show))
-            speak_text(f"Notes for the current slide: {note}")
+        # Convert PDF to images
+        images = convert_pdf_to_images("test1.pdf")
 
-            # This delay is only to let the speaking finish. Replace with real-time tracking if possible.
-            time.sleep(3)
+        # Convert PDF to text
+        slides_text = read_pdf("test1.pdf")
+
+        # Parse page range
+        if "-" in page_range:
+            start, end = map(int, page_range.split("-"))
+            pages = list(range(start, end+1))
+        else:
+            pages = list(map(int, page_range.split(',')))
+
+        # Filter based on selected pages
+        slides_text = [slides_text[i-1] for i in pages]
+        images = [images[i-1] for i in pages]
+
+        if st.button('Generate and Speak Notes'):
+            generated_notes = generate_notes(slides_text)
+
+            for i, (image, note) in enumerate(zip(images, generated_notes)):
+                col1, col2 = st.columns(2)
+                
+                with col1:
+                    st.image(image, caption=f"Slide {i+1}", use_column_width=True)
+
+                with col2:
+                    st.write(f"**Notes for Slide {i+1}**")
+                    st.write(note)
+                
+                speak_text(f"Notes for slide {i+1}: {note}")
+
+                # Wait for 3 seconds or until the user presses the skip button
+                with st.empty():
+                    for remaining in range(3, 0, -1):
+                        st.write(f"Next slide in: {remaining} seconds")
+                        time.sleep(1)
+                
+                if st.button('Skip to next slide'):
+                    continue
 
 if __name__ == '__main__':
     main()
-
-
-
-#openai.api_key = "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"
