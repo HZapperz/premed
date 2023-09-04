2a3e7aebf69ec648295d41d35c8cf1fe182ac074
Nehanth <nehanthnarendrula@gmail.com>
2023-09-04 01:46:44 -0500

    fixed it

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import pyttsx4
-engine = pyttsx4.init()
-engine.say('this is an english text to voice test.')
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
+import os
+import sys
+
+engine = pyttsx4.init('coqui_ai_tts')
+engine.setProperty('speaker_wav', './docs/i_have_a_dream_10s.wav')
+
+engine.say('this is an english text to voice test, listen it carefully and tell who i am.')
 engine.runAndWait()
